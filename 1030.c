af06344e2d1fed031534c71bea35f69f6c6d8c53
yanyuw <547274419@qq.com>
2018-01-13 11:05:51 +0800

    pat 1030

--- 1030.c
+++ 1030.c
@@ -1,41 +1,47 @@
 // 测试点4段错误
 
 #include<stdio.h>
+#include<stdlib.h>
+int cmp (const void *a, const void *b)
+{
+    return *(int *)a - *(int *)b;
+}
+
 int main()
 {
     int n,i,j;
     double p;
-    int a[10000];
+    int a[100000];
     scanf("%d%lf",&n,&p);
     for(i = 0; i < n; i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i = 0; i < n-1; i++)
-    {
-        for(j = 0; j < n-1-i; j++)
-        {
-            if(a[j] > a[j+1])
-            {
-                int tmp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = tmp;
-            }
-        }
-    }
+    qsort(a, n, sizeof(a[0]), cmp);
     int max = 0;
-    for(i = n-1; i >= 0; i--)
-    {
-        for(j = 0; j < n; j++)
-        {
-            if(a[i] <= a[j] * p)
-            {
-                if( i - j + 1 > max)
-                {
-                    max = i - j + 1;
-                }
+    // for(i = n-1; i >= 0; i--)
+    // {
+    //     for(j = 0; j < n; j++)
+    //     {
+    //         if(a[i] <= a[j] * p)
+    //         {
+    //             if( i - j + 1 > max)
+    //             {
+    //                 max = i - j + 1;
+    //             }
                 
-            }
+    //         }
+    //     }
+    // }
+    for(i = 0,j = 0; j < n; i++)
+    {
+        while(j < n && a[j] <= 1L * a[i] * p) //1强制为long型
+        {
+            j++;
+        }
+        if(max < j -i)
+        {
+            max = j -i;
         }
     }
     printf("%d",max);
